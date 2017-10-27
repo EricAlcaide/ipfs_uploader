794b6bbc4cd0adf508b9d9a58f5ae1fef373dc15
Eric Alcaide <ericalcaide1@gmail.com>
2017-10-27 22:21:36 +0200

    v0.1 now working

--- __init__.py
+++ __init__.py
@@ -1,49 +1,5 @@
 # -*- coding: iso-8859-15 -
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import jsonify
-from werkzeug.utils import secure_filename
-import ipfsapi
-import time
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-vault = 'utils/vault.txt'
-api = ipfsapi.connect('127.0.0.1', 5001)
-
-def uploadFile(post_file):
-    res = api.add(post_file)
-    post_hash = res['Hash']
-    return post_hash
-
-def saveRecord(link, filename):
-	datetime = str(time.time())
-	with open(vault, 'w+') as f:
-		f.write(datetime, filename, link)
-	return
-
-
-@app.route('/')
-def home():
-	return render_template('index.html')
-
-@app.route('/sent/',  methods = ['GET', 'POST'])
-def search():
-    file = request.files['file'] 
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(upload, filename))
-
-        prev = 'https://ipfs.io/ipfs/'
-        link = prev + uploadFile(upload+filename)
-        saveRecord(link, filename)
-
-        return render_template('sent.html', link=link)
-
-    return render_template('index.html')
-
-
+from main import *
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
---
+++ main.py
@@ -0,0 +1,44 @@
+# -*- coding: iso-8859-15 -
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+from werkzeug.utils import secure_filename
+import ipfsapi
+import os
+import time
+
+app = Flask(__name__)
+upload = 'uploads/'
+vault = 'utils/vault.txt'
+api = ipfsapi.connect('127.0.0.1', 5001)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/sent/',  methods = ['GET', 'POST'])
+def search():
+    file = request.files['file'] 
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(upload, filename))
+
+        prev = 'https://ipfs.io/ipfs/'
+        link = prev + uploadFile(upload+filename)
+        saveRecord(link, filename)
+
+        return render_template('sent.html', link=link)
+
+    return render_template('index.html')
+
+def uploadFile(post_file):
+    res = api.add(post_file)
+    post_hash = res['Hash']
+    return post_hash
+
+def saveRecord(link, filename):
+    datetime = str(time.time())
+    with open(vault, 'w+') as f:
+        f.write(datetime+','+filename+','+link)
+    return
